a8fba862ad2238e2cebdab13c024828c0981a482
Matthew Fennell <mf2017@ic.ac.uk>
2021-09-01 08:19:54 +0100

    Ignore newlines when parsing balance expressions
    It can be useful to split long balance expressions onto multiple lines,
    when you are summing over a large number of accounts. This commit
    prevents the plugin from recognising newlines as invalid characters.

--- beancount_balexpr/balexpr_test.py
+++ beancount_balexpr/balexpr_test.py
@@ -67,5 +67,90 @@ class TestBalExpr(cmptest.TestCase):
         entries, errors = balexpr(entries, options_map)
         self.assertEqual(len(errors), 5)
 
+    @loader.load_doc()
+    def test_expr_with_linux_newlines(self, entries, _, options_map):
+        """
+          plugin "beancount_balexpr.balexpr"
+          1990-01-01 open Assets:A USD
+          1990-01-01 open Assets:B USD
+          1990-01-01 open Equity:OpenBalance USD
+          1999-01-02 * "Populate accounts"
+            Assets:A 20.00 USD
+            Assets:B 10.00 USD
+            Equity:OpenBalance -30.00 USD
+          1999-01-03 custom "balexpr" "
+            Assets:A +
+            Assets:B" 30.00 USD
+        """
+        entries, _ = balexpr(entries, options_map)
+
+    @loader.load_doc()
+    def test_expr_with_windows_newlines(self, entries, _, options_map):
+        """
+          plugin "beancount_balexpr.balexpr"\r
+          1990-01-01 open Assets:A USD\r
+          1990-01-01 open Assets:B USD\r
+          1990-01-01 open Equity:OpenBalance USD\r
+          1999-01-02 * "Populate accounts"\r
+            Assets:A 20.00 USD\r
+            Assets:B 10.00 USD\r
+            Equity:OpenBalance -30.00 USD\r
+          1999-01-03 custom "balexpr" "\r
+            Assets:A +\r
+            Assets:B" 30.00 USD
+        """
+        entries, _ = balexpr(entries, options_map)
+
+    @loader.load_doc()
+    def test_expr_with_newlines_and_tabs(self, entries, _, options_map):
+        """
+          plugin "beancount_balexpr.balexpr"
+          1990-01-01 open Assets:A USD
+          1990-01-01 open Assets:B USD
+          1990-01-01 open Equity:OpenBalance USD
+          1999-01-02 * "Populate accounts"
+            Assets:A 20.00 USD
+            Assets:B 10.00 USD
+            Equity:OpenBalance -30.00 USD
+          1999-01-03 custom "balexpr" "
+          	Assets:A +
+          	Assets:B" 30.00 USD
+        """
+        entries, _ = balexpr(entries, options_map)
+
+    @loader.load_doc(expect_errors=True)
+    def test_bad_expr_with_linux_newlines(self, entries, _, options_map):
+        """
+          plugin "beancount_balexpr.balexpr"
+          1990-01-01 open Assets:A USD
+          1990-01-01 open Assets:B USD
+          1990-01-01 open Equity:OpenBalance
+          1991-01-01 pad Assets:A Equity:OpenBalance
+          1991-01-01 pad Assets:B Equity:OpenBalance
+          1991-01-02 balance Assets:A 213.00 USD
+          1991-01-02 balance Assets:B 264.00 USD
+          1991-01-03 custom "balexpr" "
+            Assets:A+
+            Assets:B" 400.00 USD
+          1991-01-03 custom "balexpr" "
+            Assets:A^
+            Assets:B" 400.00 USD
+          1991-01-03 custom "balexpr" "
+            (
+              Assets:A+
+              Assets:B" 400.00 USD
+          1991-01-03 custom "balexpr" "
+            Assets:C+
+            200" 400.00 USD
+          1991-01-03 custom "balexpr" "
+            (
+              Assets:A+
+              Assets:A
+            )*
+            Assets:B" 112464.00 CNY
+        """
+        entries, errors = balexpr(entries, options_map)
+        self.assertEqual(len(errors), 5)
+
 if __name__ == '__main__':
     unittest.main()
